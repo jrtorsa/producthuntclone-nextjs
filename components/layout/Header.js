7cd668e77d29cd4087befb0beb96bd0404d9f8d9
Raul T <jr.torsa@gmail.com>
2020-09-24 15:33:38 -0600

    adding pages

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -26,7 +26,7 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
-  const usuario = true;
+  const usuario = false;
 
   return (
     <header
@@ -71,10 +71,10 @@ const Header = () => {
             </>
           ) : (
             <>
-              <Link href="/">
+              <Link href="/login">
                 <Boton bgColor="true">Login</Boton>
               </Link>
-              <Link href="/">
+              <Link href="/crear-cuenta">
                 <Boton>Crear Cuenta</Boton>
               </Link>
             </>
