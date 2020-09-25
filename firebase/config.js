c9aac9ebbfdae54e8255c78e9c32877e93dd9c5e
Raul T <jr.torsa@gmail.com>
2020-09-25 11:17:38 -0600

    firebase config

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQUyqyKXFFiUDF2yTMJjsmujzeCw1QOm0",
+  authDomain: "product-hunt-75eb2.firebaseapp.com",
+  databaseURL: "https://product-hunt-75eb2.firebaseio.com",
+  projectId: "product-hunt-75eb2",
+  storageBucket: "product-hunt-75eb2.appspot.com",
+  messagingSenderId: "1955064188",
+  appId: "1:1955064188:web:b7f98a8000b8771858dcf9",
+};
+
+export default firebaseConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import App from "next/app";
+import firebase, { FirebaseContext } from "../firebase";
+
+const MyApp = (props) => {
+  const { Component, pageProps } = props;
+  return (
+    <FirebaseContext.Provider
+      value={{
+        firebase,
+      }}
+    >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  );
+};
 
 export default MyApp;
