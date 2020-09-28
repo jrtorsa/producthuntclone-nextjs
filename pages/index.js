72210e37cda38670b81e48374d7e5194b1cbe94e
Raul T <jr.torsa@gmail.com>
2020-09-28 13:09:46 -0600

    working on index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Layout from "../components/layout/Layout";
+import DetallesProducto from "../components/layout/DetallesProducto";
+import { FirebaseContext } from "../firebase";
 
 const Home = () => {
+  const [productos, guardarProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(manejarSnapshot);
+    };
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    guardarProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
