20bc7b8bcb42481ee41e8e4d72345ed4f6cfca2e
Raul T <jr.torsa@gmail.com>
2020-09-28 13:23:11 -0600

    adding products to index

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,4 +1,11 @@
-import Reacr from "react";
+import React from "react";
+import styled from "@emotion/styled";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { es } from "date-fns/locale";
+
+const Imagen = styled.img`
+  width: 200px;
+`;
 
 const DetallesProducto = ({ producto }) => {
   const {
@@ -15,12 +22,24 @@ const DetallesProducto = ({ producto }) => {
   return (
     <li>
       <div>
-        <div></div>
+        <div>
+          <Imagen src={urlimagen} />
+        </div>
         <div>
           <h1>{nombre}</h1>
+          <p>{descripcion}</p>
+          <div>
+            <img src="/static/img/comentario.png" />
+            <p>{comentarios.length} Comentarios</p>
+          </div>
+          <p>
+            Publicado hace:{" "}
+            {formatDistanceToNow(new Date(creado), { locale: es })}
+          </p>
         </div>
       </div>
-      <div></div>
+      <div>&#9650;</div>
+      <p>{votos}</p>
     </li>
   );
 };
