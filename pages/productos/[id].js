8aa6c0cb81abece4149139ec3d6b30d54fdb783d
Raul T <jr.torsa@gmail.com>
2020-09-29 13:08:20 -0600

    adding dynamic routing

---
+++ pages/productos/[id].js
@@ -0,0 +1,43 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { FirebaseContext } from "../../firebase";
+import Layout from "../../components/layout/Layout";
+import Error404 from "../../components/layout/404";
+
+const Producto = () => {
+  // state del componente
+  const [producto, guardarProducto] = useState({});
+  const [error, guardarError] = useState(false);
+
+  // Routing para obtner el id actual
+  const router = useRouter();
+  const {
+    query: { id },
+  } = router;
+
+  // context de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    if (id) {
+      const obtenerProducto = async () => {
+        const productoQuery = await firebase.db.collection("productos").doc(id);
+        const producto = await productoQuery.get();
+        if (producto.exists) {
+          guardarProducto(producto.data());
+        } else {
+          guardarError(true);
+        }
+      };
+      obtenerProducto();
+    }
+  }, [id]);
+
+  return (
+    <Layout>
+      <>{error && <Error404 />}</>
+    </Layout>
+  );
+};
+
+export default Producto;
